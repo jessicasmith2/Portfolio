920b9648f18877a8803a7ed34375bd315a29bb5b
jessicasmith2 <jcs.vivi@yahoo.com>
2017-12-05 20:20:35 -0800

    colors and img

--- jqjs.js
+++ jqjs.js
@@ -1,31 +1,51 @@
 $( document ).ready(function() {
 	'use strict';
 	
-	/*-----FIXED NAV PAST HEADER-----*/
-	var yourNavigation = $("#nav-container");
-    var stickyDiv = "sticky";
-    var yourHeader = $('header').height();
+	// makes the parallax elements
+function parallaxIt() {
 
-	$(window).scroll(function() {
-	  if( $(this).scrollTop() > yourHeader ) {
-		yourNavigation.addClass(stickyDiv);
-		  $("#first-break").addClass("navSpaceFill");
-	  } else {
-		yourNavigation.removeClass(stickyDiv);
-		  $("#first-break").removeClass("navSpaceFill");
-	  }
-	});
-	
-	/*-----GALLERY FUNCTIONALITY-----*/
-	$(".gallery-item").hover(function() {
-		$(this).children(".hover-cover").slideToggle();
-	});
-	
-	$(".hover-cover").on("click", function(){
-		$(this).parent().children(".gallery-content").show();
-	});
-	
-	$(".close-content").on("click", function(){
-		$(this).parent().hide();
-	});
+  // create variables
+  var $fwindow = $(window);
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // on window scroll event
+  $fwindow.on('scroll resize', function() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  }); 
+
+  // for each of content parallax element
+  $('[data-type="content"]').each(function (index, e) {
+    var $contentObj = $(this);
+    var fgOffset = parseInt($contentObj.offset().top);
+    var yPos;
+    var speed = ($contentObj.data('speed') || 1 );
+
+    $fwindow.on('scroll resize', function (){
+      yPos = fgOffset - scrollTop / speed; 
+
+      $contentObj.css('top', yPos);
+    });
+  });
+
+  // for each of background parallax element
+  $('[data-type="background"]').each(function(){
+    var $backgroundObj = $(this);
+    var bgOffset = parseInt($backgroundObj.offset().top);
+    var yPos;
+    var coords;
+    var speed = ($backgroundObj.data('speed') || 0 );
+
+    $fwindow.on('scroll resize', function() {
+      yPos = - ((scrollTop - bgOffset) / speed); 
+      coords = '40% '+ yPos + 'px';
+
+      $backgroundObj.css({ backgroundPosition: coords });
+    }); 
+  }); 
+
+  // triggers winodw scroll for refresh
+  $fwindow.trigger('scroll');
+};
+
+parallaxIt();
 });
